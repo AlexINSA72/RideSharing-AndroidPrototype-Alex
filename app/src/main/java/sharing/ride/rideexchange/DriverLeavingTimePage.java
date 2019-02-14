08456cf98460f2ba098fdb4daf3d1e7431372868
summerchenxy <chen.xiangyun@u.nus.edu>
2019-02-14 15:22:34 +0100

    driver pickup number UI hardcoded

---
+++ app/src/main/java/sharing/ride/rideexchange/DriverLeavingTimePage.java
@@ -0,0 +1,13 @@
+package sharing.ride.rideexchange;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DriverLeavingTimePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_leavingtime);
+    }
+}
