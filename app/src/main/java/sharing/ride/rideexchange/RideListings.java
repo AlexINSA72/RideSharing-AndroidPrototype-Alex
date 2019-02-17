2c7a4541f077e9112c31066b1698c2f05e81bae2
AlexINSA72 <47277046+AlexINSA72@users.noreply.github.com>
2019-02-17 19:59:44 +0100

    Changed activity ride listings

--- app/src/main/java/sharing/ride/rideexchange/RideListings.java
+++ app/src/main/java/sharing/ride/rideexchange/RideListings.java
@@ -15,13 +15,24 @@ public class RideListings extends AppCompatActivity {
 
         RecyclerView rides = (RecyclerView)findViewById(R.id.rideListings);
         rides.addItemDecoration(new DividerItemDecoration(rides.getContext(), DividerItemDecoration.VERTICAL));
-        String[] rideListings = {"Los Angeles -> San Francisco", "Santa Monica -> San Francisco",
-                "Westwood -> San Francisco", "Los Angeles -> San Jose", "Los Angeles -> Oakland"};
+        String[] rideListings = {"Los Angeles", "Santa Monica",
+                "Westwood", "San Jose", "Oakland", "San Francisco"};
 
         MyAdapter adapter = new MyAdapter(rideListings);
 
         rides.setAdapter(adapter);
 
         rides.setLayoutManager(new LinearLayoutManager(this));
+
+        RecyclerView rides2 = (RecyclerView)findViewById(R.id.rideListings2);
+        rides.addItemDecoration(new DividerItemDecoration(rides.getContext(), DividerItemDecoration.VERTICAL));
+        String[] rideListings2 = {"Los Angeles", "Santa Monica",
+                "Westwood", "San Jose", "Oakland", "San Francisco"};
+
+        MyAdapter adapter2 = new MyAdapter(rideListings2);
+
+        rides2.setAdapter(adapter2);
+
+        rides2.setLayoutManager(new LinearLayoutManager(this));
     }
 }
