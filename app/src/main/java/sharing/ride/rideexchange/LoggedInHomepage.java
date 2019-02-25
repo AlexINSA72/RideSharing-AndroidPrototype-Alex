42fe07c19f86db797b586e4115e2dfff8fe2f396
AlexINSA72 <47277046+AlexINSA72@users.noreply.github.com>
2019-02-25 23:06:34 +0100

    up

---
+++ app/src/main/java/sharing/ride/rideexchange/LoggedInHomepage.java
@@ -0,0 +1,13 @@
+package sharing.ride.rideexchange;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoggedInHomepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logged_in_homepage);
+    }
+}
